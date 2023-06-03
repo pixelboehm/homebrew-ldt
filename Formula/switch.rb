662aada19b558631b2c0531c3483ef6d2afce6fb
pixelboehm <boehnsins@gmail.com>
2023-06-03 19:12:00 +0200

    Brew formula update for ldt version v0.10.0

--- Formula/switch.rb
+++ Formula/switch.rb
@@ -5,20 +5,20 @@
 class Switch < Formula
   desc ""
   homepage "https://github.com/pixelboehm/ldt"
-  version "0.9.0"
+  version "0.10.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.9.0/switch_Darwin_x86_64.tar.gz"
-      sha256 "afba171419c05ffe221e34281c3cac584688cad69594aeb454181cb96863b94f"
+    if Hardware::CPU.arm?
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/switch_Darwin_arm64.tar.gz"
+      sha256 "bad7ff380aac2b5a14e281a6a82940f24ebf80749bfd1961a829fb45008e7804"
 
       def install
         bin.install "switch"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.9.0/switch_Darwin_arm64.tar.gz"
-      sha256 "4627d4dc78d53cd58e449a58cf3ec596890112a2254f4b75e4a3216acd33756c"
+    if Hardware::CPU.intel?
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/switch_Darwin_x86_64.tar.gz"
+      sha256 "bb375b11f48b05faa319af9cecab4f367512c31bb97a58e0b4b394ea95d818b7"
 
       def install
         bin.install "switch"
@@ -27,17 +27,17 @@ class Switch < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.9.0/switch_Linux_arm64.tar.gz"
-      sha256 "1324b72b6e7eb340c9323b36c489b4127decd2d3c2845998a323401dcc3a1689"
+    if Hardware::CPU.intel?
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/switch_Linux_x86_64.tar.gz"
+      sha256 "43df8e4086aef508a2c256c52105a015cd31d5300126a1dd22134b74e69b2095"
 
       def install
         bin.install "switch"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.9.0/switch_Linux_x86_64.tar.gz"
-      sha256 "abb6479f7cb5a855722374693481b20335a24f02f33f2acd478605528b2fc5df"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/switch_Linux_arm64.tar.gz"
+      sha256 "c47e3333ed6dd5cf0a3f6ce2baf975799b310505ffab2a39a52da39b24a03048"
 
       def install
         bin.install "switch"
