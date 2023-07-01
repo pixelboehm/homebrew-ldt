5f1046b76b06f9823520c3ffc1e108d1885edf66
pixelboehm <boehnsins@gmail.com>
2023-07-01 17:31:23 +0200

    Brew formula update for ldt version v0.10.1

--- Formula/lightbulb.rb
+++ Formula/lightbulb.rb
@@ -5,20 +5,20 @@
 class Lightbulb < Formula
   desc ""
   homepage "https://github.com/pixelboehm/ldt"
-  version "0.10.0"
+  version "0.10.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/lightbulb_Darwin_arm64.tar.gz"
-      sha256 "73d633a5503c66c19e74b2a72f16dc64d4198aee7214f372355b0620901714b2"
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.1/lightbulb_Darwin_arm64.tar.gz"
+      sha256 "95e65e9dd51a3c194c9fca222a3642a787f6a428980b7009b8a39f147f0c4f27"
 
       def install
         bin.install "lightbulb"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/lightbulb_Darwin_x86_64.tar.gz"
-      sha256 "2934e4379dde0bd527253c3eed40c2e905ff467e4aee4d22c0c521c831049a85"
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.1/lightbulb_Darwin_x86_64.tar.gz"
+      sha256 "404804642ed4e864afee52a460b5d77cdd272164796c789a1001d963f2b6ec90"
 
       def install
         bin.install "lightbulb"
@@ -28,16 +28,16 @@ class Lightbulb < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/lightbulb_Linux_x86_64.tar.gz"
-      sha256 "6893b03a79d18cc1efff1f09efdec41b9c7dd41e31e0caf4fbe086046022e325"
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.1/lightbulb_Linux_x86_64.tar.gz"
+      sha256 "7ba385401561a6eae5c6cdfc3d69268c87032625d4f873234adb14be9d683942"
 
       def install
         bin.install "lightbulb"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.0/lightbulb_Linux_arm64.tar.gz"
-      sha256 "5a4104957170782e315b352902ddb95dad2e3dd4d02798a5d1ebc3ac68dfe842"
+      url "https://github.com/pixelboehm/ldt/releases/download/v0.10.1/lightbulb_Linux_arm64.tar.gz"
+      sha256 "ece7aceda0318d9833f589a49115c4ba6b1431d8b987ca2c3b5e8c0708d12999"
 
       def install
         bin.install "lightbulb"
